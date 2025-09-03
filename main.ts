56e2cc294cd9f2212b5bcc8b3577df668116a18c
xiguapiwork <334513374@qq.com>
2025-09-04 03:20:19 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 // --- START OF FILE main.ts ---
 
-import { serve } from "https://deno.land/std@0.200.0/http/server.ts";
-import { serveDir } from "https://deno.land/std@0.200.0/http/file_server.ts";
-import { Buffer } from "https://deno.land/std@0.177.0/node/buffer.ts";
+import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
+import { Buffer } from "https://deno.land/std@0.224.0/node/buffer.ts";
 
 // --- 辅助函数：生成错误 JSON 响应 ---
 function createJsonErrorResponse(message: string, statusCode = 500) { /* ... */ }
